f6c48071d67e7390d1c5cb2a8d187cc623066b66
Harsh Rajput <harshrajput1101@gmail.com>
2024-10-15 19:22:01 +0530

    add logout functionality

--- server.js
+++ server.js
@@ -300,6 +300,10 @@ app.post('/login', async (req, res) => {
     }
 });
 
+app.post('/logout',(req,res)=>{ 
+    res.clearCookie('token'); 
+    res.status(200).send("User logout successfully"); 
+})
 
 // Start the server
 app.listen(PORT, () => {
