14ff2e6bf2db7cb890e6fcd60b662e34c4a73008
Harsh Rajput <harshrajput1101@gmail.com>
2025-01-22 00:08:33 +0530

    added some paramter in res.cookie

--- server.js
+++ server.js
@@ -423,7 +423,13 @@ app.post('/login', async (req, res) => {
 
         // Create a JWT token
         const token = jwt.sign({ email: user.email }, SECRET_KEY);
-        res.cookie("token",token);
+        res.cookie("token",token,{
+            httpOnly: false,  // Prevents client-side access to the cookie
+            secure: true,    // Ensures the cookie is only sent over HTTPS
+            sameSite: "none" ,// Required for cross-origin cookies
+            domain:"linkdinextensionbackend-dzc7dterc9cggrhd.eastus-01.azurewebsites.net"
+                    
+          });
         // Set the token as a cookie and send a successful response
         res.status(200).json({ message: "Successfully logged in", "token": token });
 
