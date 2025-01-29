5c9a67b9a8f533ee2371ccf5b5969cd06334033e
harsh474 <harshrajput1101@gmail.com>
2025-01-30 01:08:03 +0530

    Update server.js

--- server.js
+++ server.js
@@ -448,7 +448,7 @@ app.get('/logout',(req,res)=>{
 app.get('/check-login',(req,res)=>{
     const  token = req.cookies.token;
     if(token){ 
-        res.status(200).send({"message":`true,{token}`}) ;
+        res.status(200).send({"message":token}) ;
     }
     else{ 
         res.status(400).send({"message":"false,{token}"}) ;
