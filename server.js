01f1b802cb9243a84f195b796e482ce11d947120
Harsh Rajput <harshrajput1101@gmail.com>
2025-04-03 12:08:49 +0530

    initially getconfig function was not calling

--- server.js
+++ server.js
@@ -137,7 +137,12 @@ app.post('/login', async (req, res) => {
 
         const token = jwt.sign({ email: user.email }, SECRET_KEY); 
         console.log(getCookieConfig());
-        res.cookie("token", token,getCookieConfig()); 
+        res.cookie("token", token,{
+            httpOnly: true,
+            sameSite: 'none', // Required for cross-domain cookies
+            maxAge: COOKIE_MAX_AGE,
+            path: '/'
+          }); 
 
         // Set the token as a cookie and send a successful response
         res.status(200).json({ message: "Successfully logged in", "token": token });
