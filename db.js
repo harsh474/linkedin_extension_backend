4bbee8f693bba612e834243fcb26608e0fda9ad1
Harsh Rajput <harshrajput1101@gmail.com>
2025-04-10 20:38:02 +0530

    connect with azure redis

--- db.js
+++ db.js
@@ -3,11 +3,22 @@ const dotenv = require("dotenv");
 dotenv.config()
 
 // Node.js code for caching with Redis
+const HOST_NAME = process.env.HOST_NAME
+const PASSWORD   = process.env.PASSWORD
+const REDIS_PORT = process.env.REDIS_PORT
+
 const redis = require('redis');
-const redis_client = redis.createClient(); 
-const mongoose = require('mongoose')
+const redis_client = redis.createClient({
+  host: HOST_NAME,
+  port: REDIS_PORT,
+  password: PASSWORD,
+  tls: {} // Ensures SSL is used
+}); 
+const mongoose = require('mongoose');
+const { hostname } = require("os");
 let mongo_url = "mongodb://localhost:27017/email";
 mongo_url = process.env.Mongo_url
+
 
 // mongoose.connect('mongodb://localhost:27017/email')
 mongoose.connect(`${mongo_url}`)
